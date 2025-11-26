9ac6dff9e6b64b419b2313d1b57bd5c19cc97045
Alfonso-stratesys <alfonso.sanchis@stratesys-ts.com>
2025-11-26 09:21:13 +0100

    cambio guiones

--- front-mega/src/Formulario.jsx
+++ front-mega/src/Formulario.jsx
@@ -93,8 +93,8 @@ export default function App() {
 
     const salesVolumes = [
         "Pequeña - < $1.000 M",
-        "Mediana (baja) - $1.000–10.000 M",
-        "Mediana (alta) - $10.000–50.000 M",
+        "Mediana (baja) - $1.000-10.000 M",
+        "Mediana (alta) - $10.000-50.000 M",
         "Grande - > $50.000 M"
     ];
 
